4379018531977f2ad84c2f6d7c28a4396e03fab7
parth9784 <pdsharma458@gmail.com>
2024-07-25 01:44:00 +0530

    added the login signup page..

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HF_Layout from './HF_Layout';
 import S_Layout from './S_Layout';
+import LoginPage from './Login';
 import Homepage from './Homepage';
+import Signup from './Signup';
+import Blogcard from "./Blogcard";
 function App() {
   return (
         <Routes>
       <Route path="/" element={<HF_Layout> <Homepage/> </HF_Layout >}></Route>
-      {/* <Route path="/" element={<S_Layout> <Homepage/> </S_Layout >}></Route> */}
+      <Route path="/login" element={<LoginPage/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      <Route path="/blogcard" element={<Blogcard/>}></Route>
       </Routes>
   );
 }
